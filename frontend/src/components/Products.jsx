cc4b779f49418272a4bbb7505bf2d2464aacdb6d
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-04 12:04:16 +0500

    MERN_Ecommerce

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,21 +1,44 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import data from '../data'
+// import data from '../data'
 import axios from 'axios'
 
+const reducer = (state, action) => {
+    switch (action.type) {
+
+        case "FETCH_REQUEST":
+            return { ...state, loading: true }
+
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false }
+
+        case 'FETCH_FAIL':
+            return { ...state, error: action.payload, loading: false }
+
+        default:
+            return state
+    }
+}
+
 const Products = () => {
-    const [products, setProducts] = useState([])
+    // const [products, setProducts] = useState([])
+
+    const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+        loading: true,
+        error: '',
+        products: []
+    })
 
     useEffect(() => {
         const getProducts = async () => {
-            await axios.get('/api/products')
-                .then((res) => {
-                    console.log(res);
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
+            dispatch({ type: "FETCH_REQUEST" })
+            try {
+                const res = await axios.get('/api/products')
+                dispatch({ type: 'FETCH_SUCCESS', payload: res.data })
+            } catch (err) {
+                dispatch({ type: 'FETCH_FAIL', payload: err.message })
+            }
         }
         getProducts()
     }, [])
@@ -28,7 +51,7 @@ const Products = () => {
                         <h2 className='mb-5'>Product List</h2>
                     </div>
                     {
-                        data.products.map((product, index) => (
+                        products?.map((product, index) => (
                             <div key={product.slug} className="col-lg-3">
                                 <div className="cards">
                                     <Link to={`/product/${product.slug}`}>
