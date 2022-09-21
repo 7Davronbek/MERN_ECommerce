5145e8bb577444bffcc4f7a86255172b4ef2e566
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-09-21 15:44:37 +0500

    MERN_ECommerce

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ app.use("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // TEST
-app.listen(port , () => {
-    console.log(`serve at http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
